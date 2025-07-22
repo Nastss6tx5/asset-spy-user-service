64a2cf77a14965418225bcc4568329c964d9c4e9
Nastss6tx5 <Nastasiia227@gmail.com>
2025-07-22 11:32:13 +0300

    feat(STOCK-99) enabled swagger access & authorize button

--- src/main/java/asset/spy/user/service/security/SecurityConfig.java
+++ src/main/java/asset/spy/user/service/security/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final BaseJwtAuthenticationFilter jwtAuthenticationFilter;
@@ -27,6 +27,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/v1/users/profile").hasRole("USER")
                         .requestMatchers("/v1/users/**", "/v1/contacts").hasRole("ADMIN")
                         .anyRequest().authenticated())
